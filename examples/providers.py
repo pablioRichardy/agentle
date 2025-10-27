462885f75e7b144e201b29a715ddb5cc64023861
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-10-27 08:20:13 -0300

    rollback provider

--- examples/providers.py
+++ examples/providers.py
@@ -4,51 +4,46 @@ Providers Example
 This example demonstrates how to use different model providers with the Agentle framework.
 """
 
-import asyncio
 from dotenv import load_dotenv
-from rsb.models.base_model import BaseModel
 
-
+from agentle.generations.models.message_parts.file import FilePart
 from agentle.generations.models.message_parts.text import TextPart
 from agentle.generations.models.messages.user_message import UserMessage
 from agentle.generations.providers.base.generation_provider import GenerationProvider
-from agentle.generations.providers.google.google_generation_provider import (
-    GoogleGenerationProvider,
+from agentle.generations.providers.openrouter.openrouter_generation_provider import (
+    OpenRouterGenerationProvider,
 )
 
 load_dotenv()
 
 
-class PoemResponse(BaseModel):
-    poem: str
+def add_numbers(a: float, b: float) -> float:
+    return a + b
 
 
 # Example 1: Create an agent with Google's Gemini model
-provider: GenerationProvider = GoogleGenerationProvider()
+provider: GenerationProvider = OpenRouterGenerationProvider().with_fallback_models(
+    "openai/gpt-5-nano"
+)
 
+example_file = FilePart.from_local_file(
+    "/Users/arthurbrenno/Documents/Dev/Paragon/agentle/examples/harvard.wav",
+    "audio/mp3",
+)
 
-async def main() -> None:
-    # Run the Google agent
-    stream = provider.stream_async(
-        model="google/gemini-2.5-flash",
-        messages=[
-            UserMessage(
-                parts=[
-                    TextPart(
-                        text="Escreva um poema sobre o amor",
-                    ),
-                ]
-            )
-        ],
-        response_schema=PoemResponse,
-    )
+# Run the Google agent
+generation = provider.generate(
+    model="google/gemini-2.5-flash",
+    messages=[
+        UserMessage(
+            parts=[
+                example_file,
+                TextPart(
+                    text="O que tem nesse audio?",
+                ),
+            ]
+        )
+    ],
+)
 
-    async for generation in stream:
-        print(generation)
-        print()
-        print()
-        print()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+print(generation)
